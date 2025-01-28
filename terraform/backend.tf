b1a6d6230671d8f2a247cb89b1e46ff92d89a16a
frich01 <grfreire1023@gmail.com>
2025-01-27 19:39:51 -0500

    update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-bucket-012125"
-    region = "us-east-2"
-    key = "eks/terraform.tfstate"
+    bucket         = "tfstate-eks-bucket-012125"
+    dynamodb_table = "tfstate-db-bucket-012125"
+    encrypt        = true
+    key            = "dev-terraform-eks.tfstate"
+    region         = "us-east-2"
   }
 }
