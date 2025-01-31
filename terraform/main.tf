d799d669780e11d8246a8bc44e7575b7b893a1af
frich01 <grfreire1023@gmail.com>
2025-01-31 12:20:13 -0500

    updated8 main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,10 +35,10 @@ module "myapp-vpc" {
 }
 
 locals {
-  vpc_id              = module.vpc.vpc_id
-  vpc_cidr            = module.vpc.vpc_cidr_block
-  public_subnets_ids  = module.vpc.public_subnets
-  private_subnets_ids = module.vpc.private_subnets
+  vpc_id              = module.myapp-vpc.vpc_id
+  vpc_cidr            = module.myapp-vpc.vpc_cidr_block
+  public_subnets_ids  = module.myapp-vpc.public_subnets
+  private_subnets_ids = module.myapp-vpc.private_subnets
   subnets_ids         = concat(local.public_subnets_ids, local.private_subnets_ids)
 }
 
